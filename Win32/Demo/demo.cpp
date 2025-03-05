2a287535b1992196308ff749a204203c396bc13b
xiaoY <142075085+Yimyom3@users.noreply.github.com>
2025-03-05 19:02:35 +0800

    Add files via upload

--- Win32/Demo/demo.cpp
+++ Win32/Demo/demo.cpp
@@ -1,76 +1,72 @@
 #include "pch.h"
-#include <fstream>
-#pragma warning(disable : 4996)
 
-int run(int offset) {
-    HANDLE hFile = CreateFileA("C:\\Users\\lw0122106\\Desktop\\code\\C++\\DemoDll\\x64\\Release\\DemoDll.dll", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) {
-        return -1;
-    }
-    DWORD fileSize = GetFileSize(hFile, NULL);
-    HANDLE heap = HeapCreate(HEAP_CREATE_ENABLE_EXECUTE,0,0);
-    if (heap == NULL) {
-        CloseHandle(hFile);
-        return -1;
-    }
-    LPVOID buff = HeapAlloc(heap, HEAP_ZERO_MEMORY, fileSize);
-    if (buff == NULL) {
-        CloseHandle(hFile);
-        CloseHandle(heap);
-        return -1;
-    }
-    DWORD bytesRead;
-    if (ReadFile(hFile, buff, fileSize, &bytesRead, NULL)) {
-        BYTE* ReflectiveLoader = ((BYTE*)buff + offset);
-        BOOL state = ((BOOL(*)())ReflectiveLoader)();
-        CloseHandle(hFile);
-        CloseHandle(heap);
-        return state;
-    }
-    CloseHandle(hFile);
-    CloseHandle(heap);
-    return 0;
-}
-
-DWORD GetOffset() {
-    HMODULE hModule = LoadLibraryA("C:\\Users\\lw0122106\\Desktop\\code\\C++\\DemoDll\\x64\\Release\\DemoDll.dll");
-    if (hModule) {
-        return (ULONG64)GetProcAddress(hModule, "ReflectiveLoader") - (ULONG64)hModule;
-    }
-    return NULL;
-}
-
-bool SaveDllToFile() {
-    // 加载DLL并获取其基地址
-    HMODULE hModule = LoadLibraryA("C:\\Users\\lw0122106\\Desktop\\code\\C++\\DemoDll\\x64\\Release\\DemoDll.dll");
+LPVOID Getg_CiOptionsAddress() {
+    LPVOID PCiInitialize = NULL;
+    LPVOID PCipInitialize = NULL;
+    LPVOID Pg_CiOptions = NULL;
+    LPVOID PNtg_CiOptions = NULL;
+    PBYTE ptr = NULL;
+    ULONG_PTR offset = 0;
+    LPVOID drivers[1024];
+    DWORD cbNeeded, cDrivers = 0;
+    HMODULE hModule = NULL;
+    hModule = LoadLibraryExA("ci.dll", NULL, DONT_RESOLVE_DLL_REFERENCES);
     if (hModule == NULL) {
-        return false;
+        return NULL;
     }
-    // 获取模块信息
-    MODULEINFO moduleInfo;
-    if (!GetModuleInformation(GetCurrentProcess(), hModule, &moduleInfo, sizeof(MODULEINFO))) {
+    PCiInitialize = GetProcAddress(hModule, "CiInitialize");
+    if (PCiInitialize == NULL) {
         FreeLibrary(hModule);
-        return false;
+        return NULL;
     }
-    // 打开输出文件
-    ofstream outFile("C:\\Users\\lw0122106\\Desktop\\code\\C++\\2.bin", ios::binary);
-    if (!outFile.is_open()) {
+    for (WORD i = 0; i < 0x200; i++) {
+        ptr = (PBYTE)PCiInitialize + i;
+        if (*(PULONG_PTR)(ptr) == 0x8B48C78B4CCB8B4CLL) {
+            if (*(PDWORD)(ptr + 8) == 0xE8CD8BD6) {
+                PCipInitialize = ptr + *(PDWORD)(ptr + 0xC) + 0x10;
+                break;
+            }
+        }
+    }
+    if (PCipInitialize == NULL) {
         FreeLibrary(hModule);
-        return false;
+        return NULL;
     }
-    // 将模块内容写入文件
-    outFile.write(reinterpret_cast<char*>(moduleInfo.lpBaseOfDll), moduleInfo.SizeOfImage);
-    // 关闭文件和释放模块
-    outFile.close();
+    for (WORD i = 0; i < 0x200; i++) {
+        ptr = (PBYTE)PCipInitialize + i;
+        if (*(PULONG_PTR)(ptr) == 0xEC83485641544157LL) {
+            if (*(PDWORD)(ptr + 8) == 0xE98B4940) {
+                Pg_CiOptions = ptr + *(PBOOL)(ptr + 0xE) + 0x12;
+                break;
+            }
+        }
+    }
+    if (Pg_CiOptions == NULL) {
+        FreeLibrary(hModule);
+        return NULL;
+    }
+    offset = (ULONG_PTR)Pg_CiOptions - (ULONG_PTR)hModule;
     FreeLibrary(hModule);
-    return true;
+    if (EnumDeviceDrivers(drivers, sizeof(drivers), &cbNeeded) && cbNeeded < sizeof(drivers))
+    {
+        TCHAR szDriver[1024];
+        cDrivers = cbNeeded / sizeof(drivers[0]);
+        for (DWORD i = 0; i < cDrivers; i++)
+        {
+            if (GetDeviceDriverBaseName(drivers[i], szDriver, sizeof(szDriver) / sizeof(szDriver[0])))
+            {
+                if (lstrcmpiW(szDriver,L"ci.dll") == 0) {
+                    PNtg_CiOptions = (PBYTE)drivers[i] + offset;
+                    break;
+                }
+            }
+        }
+    }
+    cout << hex << offset << endl;
+    return PNtg_CiOptions;
 }
-
 
 int main() {
-    DWORD offset = GetOffset();
-    if (offset != NULL) {
-        offset -= 0xc00;
-        run(offset);
-    }
+    cout << Getg_CiOptionsAddress() << endl;
+    return 0;
 }
